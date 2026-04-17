4f75a115b2020614b924c4d0d69f5d96eb4deaab
Plisha <mm3422kk@outlook.com>
2026-04-17 18:12:51 +0500

    реализовал класс ParcelBox

--- src/ru/yandex/practicum/delivery/ParcelBox.java
+++ src/ru/yandex/practicum/delivery/ParcelBox.java
@@ -1,7 +1,30 @@
 package ru.yandex.practicum.delivery;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ParcelBox<T extends Parcel> {
-    /* метод addParcel добавление посылки в список
-        метод getAllParcels получение всех посылок
-     */
+    private final ArrayList<T> parcels = new ArrayList<>();
+    private final int maxWeight;
+
+    public ParcelBox(int maxWeight) {
+        this.maxWeight = maxWeight;
+    }
+
+    public void addParcel(T parcel) {
+        int sumWeight = 0;
+        for (T oneParcel : parcels) {
+            sumWeight += oneParcel.getWeight();
+        }
+        sumWeight += parcel.getWeight();
+        if (sumWeight <= maxWeight) {
+            parcels.add(parcel);
+        } else {
+            System.out.println("Превышен вес, посылка не добавлена!");
+        }
+    }
+
+    public List<T> getAllParcels() {
+        return parcels;
+    }
 }
