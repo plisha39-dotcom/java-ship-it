b37fd53a020255c65cc9bbda8d27a74a21feea07
Plisha <mm3422kk@outlook.com>
2026-04-18 13:08:00 +0500

    реализация 4-ех методов изменение консольного меню

--- src/ru/yandex/practicum/delivery/DeliveryApp.java
+++ src/ru/yandex/practicum/delivery/DeliveryApp.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class DeliveryApp {
 
     private static final Scanner scanner = new Scanner(System.in);
-    private static List<Trackable> trackables = new ArrayList<>();
+    private static final List<Trackable> trackables = new ArrayList<>();
     private static final ParcelBox<StandardParcel> stdParcelBox = new ParcelBox<>(10);
     private static final ParcelBox<FragileParcel> fragParcelBox = new ParcelBox<>(10);
     private static final ParcelBox<PerishableParcel> plParcelBox = new ParcelBox<>(10);
@@ -28,6 +28,12 @@ public class DeliveryApp {
                 case 3:
                     calculateCosts();
                     break;
+                case 4:
+                    contentBox();
+                    break;
+                case 5:
+                    tracking();
+                    break;
                 case 0:
                     running = false;
                     break;
@@ -39,10 +45,12 @@ public class DeliveryApp {
 
     private static void showMenu() {
         System.out.println("Выберите действие:");
-        System.out.println("1 — Добавить посылку");
-        System.out.println("2 — Отправить все посылки");
-        System.out.println("3 — Посчитать стоимость доставки");
-        System.out.println("0 — Завершить");
+        System.out.println("1 – Добавить посылку");
+        System.out.println("2 – Отправить все посылки");
+        System.out.println("3 – Посчитать стоимость доставки");
+        System.out.println("4 – Показать содержимое коробки");
+        System.out.println("5 – Показать статус отслеживаемых посылок");
+        System.out.println("0 – Завершить");
     }
 
     // реализуйте методы ниже
@@ -60,18 +68,18 @@ public class DeliveryApp {
         }
 
         System.out.println("Введите название посылки:");
-            String description = scanner.nextLine();
+        String description = scanner.nextLine();
         System.out.println("Введите вес посылки:");
-            int weight = scanner.nextInt();
-            scanner.nextLine();
+        int weight = scanner.nextInt();
+        scanner.nextLine();
         System.out.println("Введите адрес доставки:");
-            String deliveryAddress = scanner.nextLine();
+        String deliveryAddress = scanner.nextLine();
         System.out.println("Введите день отправки");
-            int sendDay = scanner.nextInt();
+        int sendDay = scanner.nextInt();
 
         switch (typeParcel) {
             case 1:
-                StandardParcel standardParcel = new StandardParcel(weight, description, deliveryAddress,sendDay);
+                StandardParcel standardParcel = new StandardParcel(weight, description, deliveryAddress, sendDay);
                 stdParcelBox.addParcel(standardParcel);
                 break;
             case 2:
@@ -85,17 +93,74 @@ public class DeliveryApp {
                 if (fragParcelBox.addParcel(fragileParcel)) {
                     trackables.add(fragileParcel);
                 }
-                    break;
+                break;
         }
         scanner.nextLine();
     }
 
     private static void sendParcels() {
-        // Пройти по allParcels, вызвать packageItem() и deliver()
+        for (Parcel oneParcel : stdParcelBox.getAllParcels()) {
+            oneParcel.packageItem();
+            oneParcel.deliver();
+        }
+        for (Parcel oneParcel : plParcelBox.getAllParcels()) {
+            oneParcel.packageItem();
+            oneParcel.deliver();
+        }
+        for (Parcel oneParcel : fragParcelBox.getAllParcels()) {
+            oneParcel.packageItem();
+            oneParcel.deliver();
+        }
     }
 
     private static void calculateCosts() {
-        // Посчитать общую стоимость всех доставок и вывести на экран
+        int sum = 0;
+        for (Parcel oneParcel : stdParcelBox.getAllParcels()) {
+            sum += oneParcel.calculateDeliveryCost();
+        }
+        for (Parcel oneParcel : plParcelBox.getAllParcels()) {
+            sum += oneParcel.calculateDeliveryCost();
+        }
+        for (Parcel oneParcel : fragParcelBox.getAllParcels()) {
+            sum += oneParcel.calculateDeliveryCost();
+        }
+        System.out.println("Общая сумма доставки: " + sum);
+    }
+
+    private static void contentBox() {
+        System.out.println("Выберите какую коробку показать:");
+        System.out.println("1 - стандартная");
+        System.out.println("2 - скоропортящаяся");
+        System.out.println("3 - хрупкая");
+        int typeBox = scanner.nextInt();
+            if (typeBox == 1) {
+                for (Parcel box : stdParcelBox.getAllParcels()) {
+                    System.out.println(box.getDescription());
+                }
+            } else if (typeBox == 2) {
+                for (Parcel box : plParcelBox.getAllParcels()) {
+                    System.out.println(box.getDescription());
+                }
+            } else if (typeBox == 3) {
+                for (Parcel box : fragParcelBox.getAllParcels()) {
+                    System.out.println(box.getDescription());
+                }
+            } else {
+                System.out.println("Неверная команда!");
+            }
+            scanner.nextLine();
+    }
+
+    private static void tracking() {
+        System.out.println("Введите новое местоположение:");
+        String newLocation = scanner.nextLine();
+        if (trackables.isEmpty()) {
+            System.out.println("Список отслеживаемых посылок пуст!");
+        } else {
+        for (Trackable trackable : trackables) {
+            trackable.reportStatus(newLocation);
+            }
+        }
     }
 }
 
